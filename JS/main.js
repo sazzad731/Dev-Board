2de811aed05b0f154e714ad69511f10035843923
sazzad731 <sajjadho731@gmail.com>
2025-03-01 18:39:44 +0600

    completed card section

--- JS/main.js
+++ JS/main.js
@@ -3,3 +3,11 @@ document.getElementById("theme-btn").addEventListener("click", function (){
   let body = document.getElementById("theme-bg");
   body.style.backgroundColor = `#${Math.floor(Math.random() * 0xFFFFFFFF).toString(16).padStart(8, '0').toUpperCase()}`
 })
+
+
+// for date
+let date = new Date().toDateString().split(" ");
+let getWeek = date.shift(0);
+document.getElementById("week").innerText = getWeek;
+let day = date.join(" ");
+document.getElementById("day").innerText = day
